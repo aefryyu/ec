0c7e91aef783afd79b37419135a65ad0c586cd07
aefryyu <imron.rivaille99@gmail.com>
2024-12-17 18:01:59 +0700

    1

---
+++ src/test/java/al/imcode/ec/EcApplicationTests.java
@@ -0,0 +1,13 @@
+package al.imcode.ec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
